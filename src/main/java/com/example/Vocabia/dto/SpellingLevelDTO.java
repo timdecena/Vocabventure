6dd14bfb1067da1b3bc66cd5d130c8f644f76c91
timgwapo <anthonycdecena@gmail.com>
2025-09-18 18:22:47 +0800

    Revert "Attempts continuation(custom wordlist)"
    This reverts commit ef805e512d71a74e7a4b063f288cfa6100f24286.

--- src/main/java/com/example/Vocabia/dto/SpellingLevelDTO.java
+++ src/main/java/com/example/Vocabia/dto/SpellingLevelDTO.java
@@ -3,37 +3,13 @@ package com.example.Vocabia.dto;
 public class SpellingLevelDTO {
     private Long id;
     private String title;
-    private int maxAttempts; 
 
-    public SpellingLevelDTO(Long id, String title, int maxAttempts) {
+    public SpellingLevelDTO(Long id, String title) {
         this.id = id;
         this.title = title;
-        this.maxAttempts = maxAttempts;
     }
 
-    // Getters
-    public Long getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public int getMaxAttempts() {
-        return maxAttempts;
-    }
-
-    // Setters
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public void setMaxAttempts(int maxAttempts) {
-        this.maxAttempts = maxAttempts;
-    }
+    // Getters and Setters
+    public Long getId() { return id; }
+    public String getTitle() { return title; }
 }
