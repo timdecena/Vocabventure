01ce6f21bd68579e36defdcdec2cea97f0f2be84
Jake Luis Lood <jakeluis.lood@cit.edu>
2025-10-30 00:16:03 +0800

    adv mode(minor fix)
    fix images in tutorial.

--- vocabia-game/src/Adventure/tutorial/battle/TutorialBattle.jsx
+++ vocabia-game/src/Adventure/tutorial/battle/TutorialBattle.jsx
@@ -1102,8 +1102,8 @@ const TutorialBattle = ({ onComplete, showQuitButton, onQuit, quitButtonText, in
         "https://images.unsplash.com/photo-1551963831-b3b1ca40c98e?w=200&h=200&fit=crop", // Food
         "https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?w=200&h=200&fit=crop", // Pancakes
         "https://images.unsplash.com/photo-1504674900247-0877df9cc836?w=200&h=200&fit=crop", // Cooking
-        "https://images.unsplash.com/photo-1565299624946-b28f40a0ca4b?w=200&h=200&fit=crop",
-        "https://images.unsplash.com/photo-1565299624946-b28f40a0ca4b?w=200&h=200&fit=crop"  // Pizza
+        "https://foodboxhq.com/wp-content/uploads/2023/02/friends-cooking-together-1.webp",
+        
       ],
       letters: "FDOOEKOCIN",
       correct: "FOOD"
